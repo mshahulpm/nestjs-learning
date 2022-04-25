4fb208de1e0fc64ddf0978348ffed34e8c551507
mshahulpm <mshahulpm@gmail.com>
2022-04-25 20:02:39 +0530

    set up two services

--- microservice/service_1/src/main.ts
+++ microservice/service_1/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule, {
+    transport: Transport.TCP,
+    options: {
+      port: 3001,
+    }
+  })
+  app.listen()
 }
 bootstrap();
